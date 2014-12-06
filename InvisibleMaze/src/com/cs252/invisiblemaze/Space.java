d42e7f241962fc0eb3806ad467f99e4585e2deb2
plohm12 <plohm12@gmail.com>
2014-12-06 15:34:21 -0500

    Removed useless code

--- InvisibleMaze/src/com/cs252/invisiblemaze/Space.java
+++ InvisibleMaze/src/com/cs252/invisiblemaze/Space.java
@@ -3,13 +3,11 @@ package com.cs252.invisiblemaze;
 public class Space {
 	private int X, Y;
 	private Space next;
-	private boolean inPath;
 	
 	public Space(int X, int Y){
 		this.X = X;
 		this.Y = Y;
 		next = null;
-		inPath = false;
 	}
 	
 	public int getX(){
@@ -26,14 +24,6 @@ public class Space {
 	
 	public Space getNext(){
 		return next;
-	}
-	
-	public void setInPath(boolean inPath){
-		this.inPath = inPath;
-	}
-	
-	public boolean isInPath(){
-		return inPath;
 	}
 	
 	public boolean equals(Object o){
