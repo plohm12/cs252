85385fc6bfd7e2728b096d6a8f2324aa8659fbab
plohm12 <plohm12@gmail.com>
2014-12-06 15:16:20 -0500

    Added Gameboard.move()

--- InvisibleMaze/src/com/cs252/invisiblemaze/Gameboard.java
+++ InvisibleMaze/src/com/cs252/invisiblemaze/Gameboard.java
@@ -11,7 +11,7 @@ public class Gameboard {
 	private static final int PATHSIZE = 16; //CHANGE IN FUTURE
 	Space start;
 	Space finish;
-	private int cX, cY; // current position X and Y values
+	Space player;
 	int gridSize;
 	ArrayList<Space> spaces;
 	
@@ -28,6 +28,7 @@ public class Gameboard {
 		System.out.println("Trying to make a new path...");
 		spaces = new ArrayList<Space>();
 		Space current = start;
+		player = start;
 		int count = 0;
 		while(count < PATHSIZE){
 			spaces.add(current);
@@ -37,19 +38,19 @@ public class Gameboard {
 			boolean valid[] = {false, false, false, false}; //key=direction, value=valid
 			int numChoices = 0;
 			//Set UP, DOWN, LEFT, RIGHT to true if each is a valid space
-			if( isValidIndex(x, y+1) && !spaces.contains(new Space(x, y+1))){
+			if( isValidSpace(x, y+1) && !spaces.contains(new Space(x, y+1))){
 				valid[UP] = true;
 				numChoices++;
 			}
-			if( isValidIndex(x, y-1) && !spaces.contains(new Space(x, y-1))){
+			if( isValidSpace(x, y-1) && !spaces.contains(new Space(x, y-1))){
 				valid[DOWN] = true;
 				numChoices++;
 			}
-			if( isValidIndex(x+1, y) && !spaces.contains(new Space(x+1, y))){
+			if( isValidSpace(x+1, y) && !spaces.contains(new Space(x+1, y))){
 				valid[RIGHT] = true;
 				numChoices++;
 			}
-			if( isValidIndex(x-1, y) && !spaces.contains(new Space(x-1, y))){
+			if( isValidSpace(x-1, y) && !spaces.contains(new Space(x-1, y))){
 				valid[LEFT] = true;
 				numChoices++;
 			}
@@ -60,11 +61,11 @@ public class Gameboard {
 				makePath();
 			}
 			
-			int[] spaceChoices = new int[numChoices]; //key=N/A, value=direction
+			int[] spaceChoices = new int[numChoices]; //key=negligible, value=direction
 			int n = 0;
-			for(int i = 0; i < 4; i++){
-				if(valid[i]){
-					spaceChoices[n] = i;
+			for(int direction = 0; direction < 4; direction++){
+				if(valid[direction]){
+					spaceChoices[n] = direction;
 					n++;
 				}
 			}
@@ -88,64 +89,50 @@ public class Gameboard {
 				finish = current;
 			}
 			count++;
-			//System.out.println("Spaces so far:");
-			//System.out.println(spaces.toString());
-			// END OF makePath()
-					
-			/*		
-			int set = 1;
-			int tries = 0;
-			while(set != 0){
-				Random random = new Random();
-				int nextX = random.nextInt(2);
-				if(nextX==0)
-					nextX= -1;
-				int nextY = random.nextInt(2);
-				if(nextY == 0)
-					nextY = -1;
-				if( isValidIndex(nextX, nextY)){
-					if( !spaces[x+nextX][y+nextY].isInPath()){
-						spaces[x][y].setNext(spaces[x+nextX][y+nextY]);
-						spaces[x+nextX][y+nextY].setInPath(true);
-						x += nextX;
-						y += nextY;
-						if(count == 7){
-							finish = spaces[x][y]; // This is the last space
-						}
-						count++;
-						set = 0;
-					}
-				}
-				tries++;
-				if(tries > 3){
-					makePath();
-				}
-			}*/
 		}
 		System.out.println("Finished at ("+finish.getX()+", "+finish.getY()+")");
 	}
 	
-	public boolean isValidIndex(int x, int y){
+	public boolean isValidSpace(int x, int y){
 		if(x < 0) return false;
 		if(y < 0) return false;
 		if(x >= gridSize) return false;
 		if(y >= gridSize) return false;
 		return true;
 	}
-	/*
-	public void performMove(int xOff, int yOff){
-		if(spaces[cX][cY].getNext() != spaces[cX+xOff][cY+yOff]){
-			// Reset to start
-			cX = 0;
-			cY = 0;
-		}else if(spaces[cX+xOff][cY+yOff] == finish){
-			// You are finished!!
-			
-		}else{
-			cX += xOff;
-			cY += yOff;
-			// Update board position on screen using coordinates
+	
+	/**
+	 * Move the player
+	 */
+	public void move(int direction){
+		int x, y;
+		switch(direction){
+		case(UP):		x = player.getX();
+						y = player.getY() + 1;
+						break;
+		case(DOWN):		x = player.getX();
+						y = player.getY() - 1;
+						break;
+		case(RIGHT):	x = player.getX() + 1;
+						y = player.getY();
+						break;
+		case(LEFT):		x = player.getX() - 1;
+						y = player.getY();
+						break;
+		default:		x = -1;
+						y = -1;
+						break;
 		}
-	}*/
+		Space temp = new Space(x, y);
+		if( !player.getNext().equals(temp))
+			player = start;
+		else{
+			player = temp;
+			if(player.equals(finish)){
+				//PLAYER HAS WON!!
+			}
+			// Update board position.
+		}
+	}
 }
 
